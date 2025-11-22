69070393da2ee3d0b0ce346a662adac8644baca2
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-22 14:18:56 +0300

    Add configuration for contract address and ABI

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONTRACT_ADDRESS = "0x2431bB3634b46dE79390CC843de2052298cB9121";
+
+const TWITTER_HANDLE = "CrazyCoderLab";
+
+const ABI = [
+    "function claimAirdrop() external",
+    "function hasClaimed(address) view returns(bool)",
+    "function claimCount() view returns(uint256)"
+];
