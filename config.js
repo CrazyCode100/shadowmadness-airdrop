53e29a677a02467638b4dc6b9be8e0bdd52a6981
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-24 04:11:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
+/* ============================================================
+   config.js — إعدادات عقد CrazyCode
+============================================================ */
+
+export const CONTRACT_ADDRESS = "0x2431bB3634b46dE79390CC843de2052298cB9121";
+
+export const CONTRACT_ABI = [
+    "function claimAirdrop() external",
+    "function hasClaimed(address) view returns (bool)"
+];
+
+/* اسم حساب تويتر */
+export const TWITTER_USERNAME = "CrazyCoderLab";
 
