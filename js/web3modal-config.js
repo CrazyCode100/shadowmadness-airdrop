d353e6bdbc7f6e61f684f4927fd85bcb76926491
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-25 08:49:18 +0300

    Update web3modal-config.js

--- js/web3modal-config.js
+++ js/web3modal-config.js
@@ -1,1 +1,13 @@
+export const providerOptions = {
+    walletconnect: {
+        package: window.WalletConnectProvider.default,
+        options: {
+            rpc: {
+                97: "https://data-seed-prebsc-1-s1.binance.org:8545"
+            }
+        }
+    }
+};
+
+export const WALLET_NETWORK = 97;
 
