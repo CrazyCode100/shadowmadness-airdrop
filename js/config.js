a7b167fd6f67c302b4ad45d4f2ec630b09595c1f
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-25 21:13:56 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-export const CONTRACT_ADDRESS = "PUT_YOUR_TOKEN_ADDRESS_HERE";
-
-export const ABI = [
-    "function claimAirdrop() external",
-    "function hasClaimed(address) view returns (bool)"
-];
-
