41345c61b786e00961c942f7b48e364a077e0313
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-25 20:03:23 +0300

    Create web3.js

---
+++ js/web3.js
@@ -0,0 +1,9 @@
+// ================================
+// إعدادات العقد
+// ================================
+
+// عنوان عقد CRAZYCODE على BSC Mainnet
+const CRAZYCODE_ADDRESS = "0xE4d658bCCBB1B8e20BD0a81a3726fDF22f1A7997";
+
+// ABI سيتم جلبه من الملف abi.js
+// CRAZYCODE_ABI يجب أن يكون معرفاً في abi.js
