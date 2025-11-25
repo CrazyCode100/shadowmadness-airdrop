5fc971fc21bda8d334d04bae271566635acde80b
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-25 08:48:41 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
+export const CONTRACT_ADDRESS = "PUT_YOUR_TOKEN_ADDRESS_HERE";
+
+export const ABI = [
+    "function claimAirdrop() external",
+    "function hasClaimed(address) view returns (bool)"
+];
 
