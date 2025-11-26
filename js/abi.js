69655949a7702b816e2013556ca16a887272cca3
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-26 13:37:50 +0300

    Update abi.js

--- js/abi.js
+++ js/abi.js
@@ -1,1 +1,8 @@
+// عقد CRAZYCODE على BSC Mainnet
+const CONTRACT_ADDRESS = "0xE4d658bCCBB1B8e20BD0a81a3726fDF22f1A7997";
+
+const CONTRACT_ABI = [
+    "function claimAirdrop() external",
+    "function hasClaimed(address) view returns (bool)"
+];
 
