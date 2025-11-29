5796f38a673a83b7c54333d51a3cfb3be823e2c6
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-29 17:39:34 +0300

    Update twitter-check.js

--- js/twitter-check.js
+++ js/twitter-check.js
@@ -1,11 +1,18 @@
-let followed = false;
+export let followConfirmed = false;
 
-document.getElementById("followBtn").onclick = () => {
+const followBtn = document.getElementById("followBtn");
+const confirmBtn = document.getElementById("confirmFollow");
+const statusBox = document.getElementById("followStatus");
+
+followBtn.onclick = () => {
     window.open("https://twitter.com/CrazyCoderLab", "_blank");
 };
 
-document.getElementById("confirmFollow").onclick = () => {
-    followed = true;
-    document.getElementById("followStatus").innerHTML =
-        "✔ تم التأكيد أنك تتابع الحساب";
+confirmBtn.onclick = () => {
+    followConfirmed = true;
+    statusBox.innerHTML = "✔ تمّ التأكيد أنك تتابع الحساب";
+    confirmBtn.classList.add("disabled");
+
+    document.getElementById("claimAirdrop").disabled = false;
+    document.getElementById("claimAirdrop").classList.remove("disabled");
 };
