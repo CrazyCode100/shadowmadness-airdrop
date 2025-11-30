afcc6a8b903eca88f0b1e37bae8dafa9d16c0fe6
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-30 21:00:06 +0300

    Delete js/abi.js

--- js/abi.js
+++
@@ -1,6 +0,0 @@
-export const CONTRACT_ADDRESS = "0xE4d658bCCBB1B8e20BD0a81a3726fDF22f1A7997";
-
-export const ABI = [
-    "function claimAirdrop() external",
-    "function hasClaimed(address) view returns (bool)"
-];
