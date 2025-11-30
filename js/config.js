fd8c1fc4f199accfde855b1f2a47862ba278932d
CrazyCode100 <shadowmadnesslab@gmail.com>
2025-11-30 20:56:32 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,19 @@
-export const CONTRACT = "0xE4d658bCCBB1B8e20BD0a81a3726fDF22f1A7997";
+// إعدادات عامة للتطبيق (كـ متغيرات عالمية)
 
-export const ABI = [
-    "function claimAirdrop() external",
-    "function hasClaimed(address) view returns (bool)"
-];
+window.CRAZY_CONFIG = {
+    CONTRACT: "0xE4d658bCCBB1B8e20BD0a81a3726fDF22f1A7997", // عقد CRAZYCODE على BSC
+    ABI: [
+        "function claimAirdrop() external payable",
+        "function hasClaimed(address) view returns (bool)"
+    ],
+    TWITTER: "CrazyCoderLab",
+    REQUIRED_GAS_BNB: "0.0001" // قيمة BNB المطلوبة مع المطالبة
+};
 
-export const TWITTER = "CrazyCoderLab";
-
-export const REQUIRED_GAS = "0.0001";
+// حالة الجلسة
+window.CRAZY_STATE = {
+    provider: null,
+    signer: null,
+    userAddress: null,
+    followed: false
+};
